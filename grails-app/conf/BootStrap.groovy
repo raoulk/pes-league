7f6f07b2d69e41c613c88bf184a92a810c182ef5
Raoul Kaltenhaeuser <raoul@kaltenhaeuser.de>
2015-05-28 18:05:25 +0200

    player creation

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,25 +3,19 @@ import de.raoulk.pesleague.Tournament;
 
 class BootStrap {
 
-    def init = { servletContext ->
-		Player raoul = new Player(name: 'Steyn')
-		Player olt = new Player(name: 'Olt')
-		Player peez = new Player(name: 'Peez')
-		Player jo = new Player(name: 'Jo')
-		raoul.save()
-		olt.save()
-		peez.save()
-		jo.save()
-		
+	def init = { servletContext ->
 		Tournament tournament = new Tournament()
-		tournament.addToPlayers(raoul)
-		tournament.addToPlayers(jo)
-		tournament.addToPlayers(peez)
-		tournament.addToPlayers(olt)
+		
+		for(playerName in ['Steyn','Olt','Peez']){
+			Player player = new Player(name: playerName)
+			player.save()
+			tournament.addToPlayers(player)
+		}
 		
 		tournament.createMatches()
 		tournament.save()
-    }
-    def destroy = {
-    }
+	}
+
+	def destroy = {
+	}
 }
